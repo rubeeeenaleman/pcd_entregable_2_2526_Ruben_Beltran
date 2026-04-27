fbaf906d0e95d93a375cff6cdd100ff96fc6b40b
beltranjimenez-edu-upct-es <beltran.jimenez@edu.upct.es>
2026-04-27 21:35:20 +0200

    Comenzando con el código

---
+++ sistema_musical.py
@@ -0,0 +1,7 @@
+class Cancion:
+    def __init__(self, titulo : str, fecha : str, atributos_sonoros : dict, atributos_sentimentales : dict):
+        self.titulo = titulo
+        self.fecha = fecha
+        self.atributos_sonoros = atributos_sonoros
+        self.atributos_sentimentales = atributos_sentimentales
+        
