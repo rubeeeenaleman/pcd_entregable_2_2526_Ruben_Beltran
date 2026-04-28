1b4e597251b1d79e119c48d309bd9e7f2c500d8f
beltranjimenez-edu-upct-es <beltran.jimenez@edu.upct.es>
2026-04-28 18:57:48 +0200

    Empezando la definición de clases

--- sistema_musical.py
+++ sistema_musical.py
@@ -4,4 +4,13 @@ class Cancion:
         self.fecha = fecha
         self.atributos_sonoros = atributos_sonoros
         self.atributos_sentimentales = atributos_sentimentales
-        
+
+class Cantante:
+    def __init__(self, nombre : str, fecha_nacimiento : str, atributos_sonoros : dict, atributos_sentimentales : dict):
+        self.nombre = nombre
+        self.fecha_nacimiento = fecha_nacimiento
+
+    def calcular_atrb_sonoros():
+        pass
+    
+    def 
